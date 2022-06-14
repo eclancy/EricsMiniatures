1e5979241209c6c1696323e3d8b63950020abf03
Eric Clancy <eric.clancy@maine.edu>
2022-06-14 12:55:55 -0400

    added trex, brain collector, missing model kits, and added model kit section on home page

--- src/Components/Shared/Constants.js
+++ src/Components/Shared/Constants.js
@@ -1,11 +1,11 @@
-import GirlOnBeach from '../../Images/Miniatures/GirlOnBeach/GirlOnBeach1.jpg';
+import TRex from '../../Images/Miniatures/TRex/Trex1.jpg';
 import Temple6 from '../../Images/Terrain/TempleInterior/Temple6.jpg';
 import GameAnimation from '../../Images/Other/WizardWars/WizardWarsFireGif.gif'
+import GundamCentaur3 from '../../Images/ModelKits/GundamCentaur/GundamCentaur3.jpg'
 
 export const sections = [
    {
     title: 'Miniatures',
-    date: 'Aug 4',
     description:
       "Hand painted miniature creatures, characters, and props. Monsters that can fit in the palm of your hand!",
     image: GirlOnBeach,
@@ -13,15 +13,20 @@ export const sections = [
   },
   {
     title: 'Terrain',
-    date: 'Aug 4',
     description:
       "Sculpted from a wide variety of items including clay, foam, or even trash! Realistic landscaps crafted and painted from basic every day items.",
     image: Temple6,
     url: '/gallery/terrain',
   },
   {
+    title: 'Model Kits',
+    description:
+      "Model kits involve tiny plastic parts and following instructions. Not as creative as other projects, but oh so satisfying.",
+    image: GundamCentaur3, 
+    url: '/gallery/modelkits'
+  },
+  {
     title: 'Other Projects',
-    date: 'Aug 4',
     description:
       "Other projects I've worked on, including 3d printing, creating games, and anything else I think is cool enough to share.",
     image: GameAnimation, 
