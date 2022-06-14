b95b3d638b20b659b87b67fae472b29e2e58beb0
Eric Clancy <eric.clancy@maine.edu>
2022-06-14 13:03:19 -0400

    fixed preview images on home

--- src/Components/Shared/Constants.js
+++ src/Components/Shared/Constants.js
@@ -1,35 +1,35 @@
-import TRex from '../../Images/Miniatures/TRex/Trex1.jpg';
-import Temple6 from '../../Images/Terrain/TempleInterior/Temple6.jpg';
-import GameAnimation from '../../Images/Other/WizardWars/WizardWarsFireGif.gif'
-import GundamCentaur3 from '../../Images/ModelKits/GundamCentaur/GundamCentaur3.jpg'
+import miniaturePreview from '../../Images/Miniatures/Blimpy/Blimpy2.jpg';
+import terrainPreview from '../../Images/Terrain/TempleInterior/Temple6.jpg';
+import modelkitPreview from '../../Images/ModelKits/GundamCentaur/GundamCentaur3.jpg'
+import otherPreview from '../../Images/Other/WizardWars/WizardWarsFireGif.gif'
 
 export const sections = [
    {
     title: 'Miniatures',
     description:
       "Hand painted miniature creatures, characters, and props. Monsters that can fit in the palm of your hand!",
-    image: GirlOnBeach,
+    image: miniaturePreview,
     url: '/gallery/miniatures',
   },
   {
     title: 'Terrain',
     description:
       "Sculpted from a wide variety of items including clay, foam, or even trash! Realistic landscaps crafted and painted from basic every day items.",
-    image: Temple6,
+    image: terrainPreview,
     url: '/gallery/terrain',
   },
   {
     title: 'Model Kits',
     description:
       "Model kits involve tiny plastic parts and following instructions. Not as creative as other projects, but oh so satisfying.",
-    image: GundamCentaur3, 
+    image: modelkitPreview, 
     url: '/gallery/modelkits'
   },
   {
     title: 'Other Projects',
     description:
       "Other projects I've worked on, including 3d printing, creating games, and anything else I think is cool enough to share.",
-    image: GameAnimation, 
+    image: otherPreview, 
     url: '/gallery/other'
   },
 ];
